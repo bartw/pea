7b7cd045f76fb738c35d10704a75444c87f3e576
Bart Wijnants <bartwijnants@gmail.com>
2023-03-04 12:00:18 +0000

    Move Head to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>pea</title>
+        <meta name="description" content="pea" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
-import Head from "next/head";
 import Image from "next/image";
 
 const Home = () => (
   <>
-    <Head>
-      <title>pea</title>
-      <meta name="description" content="pea" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.svg" />
-    </Head>
     <main>
       <h1>pea</h1>
       <Image src="/pea.svg" alt="pea logo" width={180} height={37} priority />
